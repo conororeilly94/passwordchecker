16527fcd380b65bf2cd2d37c2997f4c54e8fbcea
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-02-22 20:06:25 +0000

    final version

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 def request_api_data(query_char): # Give hash version of password
 	url = 'https://api.pwnedpasswords.com/range/' + query_char #Only gives part of hash function password
@@ -8,9 +9,28 @@ def request_api_data(query_char): # Give hash version of password
 		raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
 	return res
 
+def get_password_leaks_count(hashes, hash_to_check):
+	hashes = (line.split(':') for line in hashes.text.splitlines())
+	for h, count in hashes:
+		if h == hash_to_check:
+			return count
+	return 0
+
 def pwned_api_check(password):
 	# Check password if it exits in API response
 	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-	return sha1password 
+	first5_char, tail = sha1password[:5], sha1password[5:]
+	response = request_api_data(first5_char)
+	return get_password_leaks_count(response, tail)
 
-pwned_api_check('123')
+def main(args):
+	for password in args:
+		count = pwned_api_check(password)
+		if count:
+			print(f'{password} was found {count} times. You should change your password')
+		else:
+			print(f'{password} was not found. Carry on')
+	return 'done'
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
